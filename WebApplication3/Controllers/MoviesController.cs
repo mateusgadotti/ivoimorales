1ae160e216f88707ecaa21e9dc56d1f8a105f3be
mateusgadotti <mateus.gadotti@catolicasc.org.br>
2017-08-08 21:15:50 -0300

    dasdASDASDaSd

---
+++ WebApplication3/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class MoviesController : Controller
+    {
+        //
+        // GET: /Movie/
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Meu amigo" };
+            return View(movie);
+        }
+	}
+}
